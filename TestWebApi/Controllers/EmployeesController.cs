e5f9a3ba5dcb92acbb4a5dd92ac7a63d73b8dcca
Dmitry998 <40994829+Dmitry998@users.noreply.github.com>
2020-11-05 01:47:08 +0300

    1

--- TestWebApi/Controllers/EmployeesController.cs
+++ TestWebApi/Controllers/EmployeesController.cs
@@ -25,18 +25,77 @@ namespace TestWebApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployees()
         {
+            //var players = db.Players.Include(p => p.Team); это не от сюда даже
+
+
+            //var employees = db.Employees.Include(e => e.Company);
+            //return Ok(employees);
             return await db.Employees.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Employee>> GetConcreteEmployee(int id)
+        public async Task<ActionResult<Employee>> GetEmployeesThisCompany(int id)
         {
-            Employee employee = await db.Employees.FirstOrDefaultAsync(x => x.Id == id);
-            if (employee == null)
+            //Employee employee = await db.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            //if (employee == null)
+            // return NotFound();
+
+
+            List<Employee> employeesThisCompany = new List<Employee>();
+            foreach (Employee em in await db.Employees.ToListAsync())
+            {
+                if (em.CompanyId == id)
+                    employeesThisCompany.Add(em);
+
+            }
+            if (employeesThisCompany.Count == 0)
                 return NotFound();
-            return new ObjectResult(employee);
+            return Ok(employeesThisCompany);
         }
 
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<Employee>> GetNameCompany(int id)
+        //{
+        //    //Employee employee = await db.Employees.FirstOrDefaultAsync(x => x.Id == id);
+        //    //if (employee == null)
+        //    // return NotFound();
+
+        //    Company company = await db.Companies.FirstOrDefaultAsync(x => x.Id == id);
+        //    if (company == null)
+        //        return NotFound();
+        //    return new ObjectResult(company);
+
+        //    //List<Employee> employeesThisCompany = new List<Employee>();
+        //    //foreach (Employee em in await db.Employees.ToListAsync())
+        //    //{
+        //    //    if (em.CompanyId == id)
+        //    //        employeesThisCompany.Add(em);
+
+        //    //}
+        //    //if (employeesThisCompany.Count == 0)
+        //    //    return NotFound();
+        //    //return Ok(employeesThisCompany);
+        //}
+
+        //[HttpPost]
+        //public async Task<ActionResult<Company>> GetNameCompany(string name)
+        //{
+        //    //Company company = await db.Companies.FirstOrDefaultAsync(x => x.Id == id);
+        //    //if (company == null)
+        //    //    return NotFound();
+        //    //return new ObjectResult(company);
+
+        //    List<Employee> employeesThisCompany = new List<Employee>();
+        //    var employees = db.Employees.Include(e => e.Company);
+        //    foreach (var em in employees)
+        //    {
+        //        if (em.Company.Name == name)
+        //            employeesThisCompany.Add(em);
+        //    }
+        //    if (employeesThisCompany.Count == 0)
+        //        return NotFound();
+        //    return Ok(employeesThisCompany);
+        //}
 
     }
 }
--- TestWebApi/Startup.cs
+++ TestWebApi/Startup.cs
@@ -7,18 +7,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using TestWebApi.Models;
 
 namespace TestWebApi
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
+            string con = "Server=(localdb)\\mssqllocaldb;Database=db_companies;Trusted_Connection=True;";
+            // устанавливаем контекст данных
+            services.AddDbContext<EmployeesContext>(options => options.UseSqlServer(con));
+
+            services.AddControllers(); // используем контроллеры без представлений
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -30,10 +35,7 @@ namespace TestWebApi
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();  // подключаем маршрутизацию на контроллеры
             });
         }
     }
