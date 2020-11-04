2e58c85b8e02f12bf175ba359bb1746b971871bb
Dmitry998 <40994829+Dmitry998@users.noreply.github.com>
2020-11-04 20:58:47 +0300

    Добавьте файлы проекта.

---
+++ TestWebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestWebApi.Models;
+
+namespace TestWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        EmployeesContext db;
+
+        public EmployeesController(EmployeesContext context)
+        {
+            db = context;
+
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployees()
+        {
+            return await db.Employees.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetConcreteEmployee(int id)
+        {
+            Employee employee = await db.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            if (employee == null)
+                return NotFound();
+            return new ObjectResult(employee);
+        }
+
+
+    }
+}
---
+++ TestWebApi/Models/EmployeesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestWebApi.Models
+{
+    public class EmployeesContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public EmployeesContext(DbContextOptions<EmployeesContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
