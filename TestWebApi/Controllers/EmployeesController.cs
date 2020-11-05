583ddf63c5f1654bdfd896ecd012b79741d6cd1b
Dmitry998 <40994829+Dmitry998@users.noreply.github.com>
2020-11-06 01:47:09 +0300

    Добавил контроллер для паспортов

--- TestWebApi/Controllers/EmployeesController.cs
+++ TestWebApi/Controllers/EmployeesController.cs
@@ -32,35 +32,15 @@ namespace TestWebApi.Controllers
                 .Include(e => e.Passports)
                 .ToListAsync();
             return Ok(employees);
-
-
-            //return await db.Employees.ToListAsync();
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Employee>> GetEmployeesThisCompany(int id)
         {
-            //Employee employee = await db.Employees.FirstOrDefaultAsync(x => x.Id == id);
-            //if (employee == null)
-            //    return NotFound();
+            Company thisCompany = await db.Companies.FirstOrDefaultAsync(c => c.Id == id);
 
-
-            //Passport passport = await db.Passports.Include(p => p.Employee).FirstOrDefaultAsync(x => x.Id == id);
-            //if (passport == null)
-            //    return NotFound();
-            //return new ObjectResult(passport);
-
-            //List<Passport> passportsThisEmployeeId = new List<Passport>();
-
-            //foreach(var p in await db.Passports.Include(p => p.Employee.Company).ToListAsync())
-            //{
-            //    if (p.EmployeeId == id)
-            //        passportsThisEmployeeId.Add(p);
-            //}
-            //if (passportsThisEmployeeId.Count == 0)
-            //    return NotFound();
-            //return Ok(passportsThisEmployeeId);
-
+            if (thisCompany == null)
+                return NotFound();
 
             List<Employee> employeesThisCompany = new List<Employee>();
             foreach (Employee em in await db.Employees
@@ -70,8 +50,6 @@ namespace TestWebApi.Controllers
                 if (em.CompanyId == id)
                     employeesThisCompany.Add(em);
             }
-            if (employeesThisCompany.Count == 0)
-                return NotFound();
             return Ok(employeesThisCompany);
         }
 
@@ -79,59 +57,42 @@ namespace TestWebApi.Controllers
         [HttpPost]
         public async Task<ActionResult<Employee>> AddEmpolyee(Employee employee)
         {
-            //if (!ModelState.IsValid)
-            //{
-            //    return BadRequest(ModelState);
-            //}
-
             db.Employees.Add(employee);
+            await db.SaveChangesAsync();
+            return Ok(employee.Id); // возвращаем id добавленного пользователя
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> DeleteEmployee(int id)
+        {
+            Employee employee = db.Employees.FirstOrDefault(e => e.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            db.Employees.Remove(employee);
+            await db.SaveChangesAsync();
+            return Ok(employee.Id); // возвращаем id удаленного пользователя.
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Employee>> Put(Employee employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest();
+            }
+
+            if (!db.Employees.Any(e => e.Id == employee.Id))
+            {
+                return NotFound();
+            }
+
+            //Employee employeeInDb = db.Employees.FirstOrDefault(e => e.Id == employee.Id);
+            db.Update(employee);
             await db.SaveChangesAsync();
             return Ok(employee);
         }
-
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Employee>> GetNameCompany(int id)
-        //{
-        //    //Employee employee = await db.Employees.FirstOrDefaultAsync(x => x.Id == id);
-        //    //if (employee == null)
-        //    // return NotFound();
-
-        //    Company company = await db.Companies.FirstOrDefaultAsync(x => x.Id == id);
-        //    if (company == null)
-        //        return NotFound();
-        //    return new ObjectResult(company);
-
-        //    //List<Employee> employeesThisCompany = new List<Employee>();
-        //    //foreach (Employee em in await db.Employees.ToListAsync())
-        //    //{
-        //    //    if (em.CompanyId == id)
-        //    //        employeesThisCompany.Add(em);
-
-        //    //}
-        //    //if (employeesThisCompany.Count == 0)
-        //    //    return NotFound();
-        //    //return Ok(employeesThisCompany);
-        //}
-
-        //[HttpPost]
-        //public async Task<ActionResult<Company>> GetNameCompany(string name)
-        //{
-        //    //Company company = await db.Companies.FirstOrDefaultAsync(x => x.Id == id);
-        //    //if (company == null)
-        //    //    return NotFound();
-        //    //return new ObjectResult(company);
-
-        //    List<Employee> employeesThisCompany = new List<Employee>();
-        //    var employees = db.Employees.Include(e => e.Company);
-        //    foreach (var em in employees)
-        //    {
-        //        if (em.Company.Name == name)
-        //            employeesThisCompany.Add(em);
-        //    }
-        //    if (employeesThisCompany.Count == 0)
-        //        return NotFound();
-        //    return Ok(employeesThisCompany);
-        //}
 
     }
 }
---
+++ TestWebApi/Controllers/PassportsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestWebApi.Models;
+
+namespace TestWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PassportsController : ControllerBase
+    {
+        EmployeesContext db;
+
+        public PassportsController(EmployeesContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllPassports()
+        {
+            var passports = await db.Passports.ToListAsync();
+            return Ok(passports);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetPassportsThisEmployee(int id)
+        {
+            Employee thisEmployee = await db.Employees
+                .Include(e => e.Passports)
+                .FirstOrDefaultAsync(e => e.Id == id);
+
+            if(thisEmployee == null)
+                return NotFound();
+
+            List<Passport> passportsThisEmployee = thisEmployee.Passports.ToList();
+            return Ok(passportsThisEmployee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> AddPasport(Passport passport)
+        {
+            if (!db.Employees.Any(e => e.Id == passport.EmployeeId)) // Если нет сотрудника с таким id
+                return BadRequest();
+
+            db.Passports.Add(passport);
+            await db.SaveChangesAsync();
+            return Ok(passport); // возвращаем добавленный паспорт
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Passport>> DeletePassport(int id)
+        {
+            Passport passport = db.Passports.FirstOrDefault(e => e.Id == id);
+            if (passport == null)
+            {
+                return NotFound();
+            }
+            db.Passports.Remove(passport);
+            await db.SaveChangesAsync();
+            return Ok(passport); // возвращаем удаленный паспорт.
+        }
+    }
+}
